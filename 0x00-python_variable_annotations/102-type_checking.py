d7ec844a7c62bf30ff51fc3ebd011aba2ab59031
uwen_godwin <uwengodwin001@gmail.com>
2024-07-05 16:22:21 +0100

    Python variable annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,20 +4,20 @@
 A module for type checking.
 """
 
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
     """
     Returns a list with elements repeated by the factor.
     
     Parameters:
-    - lst: A tuple of integers.
+    - lst: A tuple of any type of elements.
     - factor: An integer factor to repeat elements (default is 2).
     
     Returns:
-    - A list of integers with each element repeated by the factor.
+    - A list of elements with each element repeated by the factor.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List[Any] = [
         item for item in lst
         for _ in range(factor)
     ]
@@ -27,3 +27,7 @@ array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 zoom_3x = zoom_array(array, 3)
+
+if __name__ == "__main__":
+    print(zoom_2x)
+    print(zoom_3x)
