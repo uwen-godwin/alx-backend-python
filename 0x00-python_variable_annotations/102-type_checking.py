4a2bde1eaecb44bd61b1e75ca3ad824b5680ecf0
uwen_godwin <uwengodwin001@gmail.com>
2024-07-05 16:15:19 +0100

    Python variable annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
+"""
+A module for type checking.
+"""
+
 from typing import Tuple, List
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Returns a list with elements repeated by the factor.
+    """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
-
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3) 
+zoom_3x = zoom_array(array, 3)
