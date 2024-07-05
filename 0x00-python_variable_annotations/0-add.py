8ba577b3ca5511dc81e1de9552618c810206c0b7
uwen_godwin <uwengodwin001@gmail.com>
2024-07-05 16:45:52 +0100

    Python variable annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,5 +3,12 @@
 def add(a: float, b: float) -> float:
     """
     Adds two floats and returns their sum.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        float: The sum of a and b.
     """
     return a + b
