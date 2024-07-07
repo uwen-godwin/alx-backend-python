376b0a45604a3bbedaddd0bb700ae467822de4a1
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 15:15:22 +0100

    python variable annotation

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -9,6 +9,7 @@ def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     Returns the first element of a sequence if it exists, else None.
     """
+    
     if lst:
         return lst[0]
     else:
