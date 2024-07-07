88ba6ca84f11f65ebdc9ddf5416a5967d7436b8e
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 15:22:57 +0100

    python variable annotation

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,6 +6,7 @@ A module for type checking.
 
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Returns a list with elements repeated by the factor.
