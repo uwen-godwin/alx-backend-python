89990cbc403dad925a7a868a578ddda55143419e
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 15:09:10 +0100

    python variable annotation

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""function make_multiplier that takes a float multiplier as argument
+"""Function make_multiplier that takes a float multiplier as an argument
 and returns a function that multiplies a float by multiplier."""
+
 from typing import Callable
 
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """ function make_multiplier that takes a float multiplier as argument"""
+    """Function make_multiplier that takes a float multiplier as an argument."""
     def multiply(x: float) -> float:
-        return multiplier * multiplier
+        return x * multiplier
     return multiply
+
+# Example usage
+if __name__ == "__main__":
+    times_three = make_multiplier(3.0)
+    print(times_three(10)) 
