7d356a89e169d28df70844bf92dcfa577323b0f5
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 14:20:01 +0100

    lockboxes

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,10 +6,12 @@ Module for basic annotations - add
 def add(a: float, b: float) -> float:
     """
     Function to add two floats.
-    Arguments:
-    a -- first float
-    b -- second float
+    
+    Args:
+    a (float): The first float.
+    b (float): The second float.
+    
     Returns:
-    Sum of a and b as a float
+    float: The sum of a and b.
     """
     return a + b
