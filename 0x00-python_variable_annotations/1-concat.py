b2286b8b9f5f45dfc33ab2d1aa062e9411105c79
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 14:32:04 +0100

    lockboxes

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""
-A module for basic variable annotations.
-"""
+"""Module which has a function to concatenate two strings and returns a single
+String value"""
+
 
 def concat(str1: str, str2: str) -> str:
-    """
-    Concatenates two strings.
-    """
+    """adds two strings together and returns a joint string"""
     return str1 + str2
