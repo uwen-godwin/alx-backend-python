0a543bef0bef6734da5be6c5c08066b90849d2cc
uwen_godwin <uwengodwin001@gmail.com>
2024-07-07 15:00:34 +0100

    python variable annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""
-A module for sum of list of floats.
-"""
+"""function sum_list which takes a list input_list of floats
+as argument and returns their sum as a float."""
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
-    """
-    Returns the sum of a list of floats.
-    """
-    return sum(input_list)
+    """Takes a list of float numbers and sum it up and returns a
+    float value"""
+    count: float = 0
+    for i in input_list:
+        count += i
+    return count
