9caf7c82948b6c1d1c54e73fe8271e7766cbf97d
uwen_godwin <uwengodwin001@gmail.com>
2024-07-10 13:54:49 +0100

    python_async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,18 +3,13 @@
 Coroutine that collects 10 random numbers using an async
 comprehension over async_generator, then returns the 10 random numbers.
 """
-
 import asyncio
-from typing import List
+import random
+from typing import Generator
 
-async_generator = __import__('0-async_generator').async_generator
 
-
-async def async_comprehension() -> List[float]:
-    """
-    Collects 10 random numbers from an async generator function.
-    Returns:
-        List[float]: A list of 10 random float numbers.
-    """
-    random_numbers = [i async for i in async_generator()]
-    return random_numbers
+async def async_generator() -> Generator[float, None, None]:
+    """Coroutine that yields a random number between 0 and 10, ten times."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
